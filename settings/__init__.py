000826e7bbc6c34c331d03f06cd4533d464a5e53
RomainCtl <romain.chantrel35@gmail.com>
2020-09-22 15:05:35 +0200

    Add jwt secret key and change debug var (for flask)

--- settings/__init__.py
+++ settings/__init__.py
@@ -6,5 +6,6 @@ load_dotenv()
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SQLALCHEMY_DATABASE_URI = 'postgresql://'+os.environ['DB_USER_LOGIN']+':'+os.environ['DB_USER_PASSWORD']+'@'+os.environ['DB_URL']+':'+os.environ['DB_PORT']+'/'+os.environ['DB_NAME']
 
-DEBUG = os.environ['DEBUG_MODE']
+JWT_SECRET_KEY = os.environ['SECRET_KEY']
+
 PORT = os.environ['SERVICE_PORT']
