a7f4c0fcdd1452b93e21dbc27f6c222b66384f5b
RomainCtl <romain.chantrel35@gmail.com>
2020-09-26 17:04:34 +0200

    Update game schema and dto

--- src/resources/game/dto.py
+++ src/resources/game/dto.py
@@ -1,24 +1,24 @@
 from flask_restx import Namespace, fields
 
+
 class GameDto:
     api = Namespace("game", description="Game related operations.")
     game = api.model(
         "Game object",
         {
-            "uid": fields.String,
+            "game_id": fields.String,
+            "steamid": fields.String,
             "name": fields.String,
             "icon_url": fields.String,
-            "rating": fields.Float,
-            "rating_count": fields.Integer,
-            "price": fields.Float,
-            "in_app_purchases": fields.Float,
-            "description": fields.String,
-            "developer": fields.String,
-            "languages": fields.String,
-            "size": fields.Integer,
-            "primary_genre": fields.String,
+            "short_description": fields.String,
+            "header_image": fields.String,
+            "website": fields.String,
+            "developers": fields.String,
+            "publishers": fields.String,
+            "price": fields.String,
             "genres": fields.String,
-            "original_release_date": fields.String,
+            "recommendations": fields.String,
+            "release_date": fields.String,
         },
     )
 
