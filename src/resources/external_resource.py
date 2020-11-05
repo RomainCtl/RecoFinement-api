0c16110ccc2e1a52c7cfa33c2029c08afe0a8c90
RomainCtl <romain.chantrel35@gmail.com>
2020-11-05 17:37:01 +0100

    fix some ...

--- src/resources/external_resource.py
+++ src/resources/external_resource.py
@@ -2,18 +2,17 @@ from flask import request, current_app
 from flask_restx import Resource
 from flask_jwt_extended import jwt_required, get_jwt_identity, decode_token
 
-import asyncio
 from threading import Thread
 
 # External service modules
 from src.service import ExternalService
 from src.dto import ExternalDto
 
-#from src.schemas import UpdateExternalDataSchema
 from src.utils import validation_error
 
 api = ExternalDto.api
 oauth_external = ExternalDto.oauth_url
+
 
 @api.route("/spotify")
 class ExternalSpotifyResource(Resource):
@@ -32,9 +31,10 @@ class ExternalSpotifyResource(Resource):
 
         return ExternalService.get_spotify_oauth(user_uuid)
 
+
 @api.route("/spotify/callback")
 class ExternalSpotifyCallbackResource(Resource):
-    #oauth_external = ExternalDto.oauth_callback # TODO when front is done
+    # oauth_external = ExternalDto.oauth_callback # TODO when front is done
     @api.doc(
         "Spotify Oauth2 Callback",
         responses={
@@ -43,19 +43,21 @@ class ExternalSpotifyCallbackResource(Resource):
             404: "User not found!",
         }
     )
-    
-    #@api.expect(oauth_external, validate=True) # TODO when front is done
-    #@jwt_required
-    @api.doc(security=None)# TODO mettre jwt_required lorsque le front est fait
+    # @api.expect(oauth_external, validate=True) # TODO when front is done
+    # @jwt_required
+    # TODO mettre jwt_required lorsque le front est fait
+    @api.doc(security=None)
     def get(self):
         """ Get access and refresh tokens """
         csrf = request.args.get('state')
-        user_uuid = decode_token(csrf)['identity']# TODO change after front done  : get_jwt_identity()
-        code = request.args.get('code') # TODO wait  front  -> receive "code" in json
-        
-        res = ExternalService.spotify_callback(csrf,code,user_uuid)
-        thread = Thread(target=ExternalService.get_spotify_data, args=(user_uuid,current_app._get_current_object()))
+        # TODO change after front done  : get_jwt_identity()
+        user_uuid = decode_token(csrf)['identity']
+        # TODO wait  front  -> receive "code" in json
+        code = request.args.get('code')
+
+        res = ExternalService.spotify_callback(csrf, code, user_uuid)
+        thread = Thread(target=ExternalService.get_spotify_data, args=(
+            user_uuid, current_app._get_current_object()))
         thread.daemon = True
         thread.start()
         return res
-
--- src/service/external_service.py
+++ src/service/external_service.py
@@ -5,7 +5,6 @@ from flask import current_app
 from flask_jwt_extended import create_access_token, decode_token
 
 import dateutil.parser
-import asyncio
 import sys
 
 
@@ -15,10 +14,8 @@ class ExternalService:
         """ Get spotify oauth url """
         if not (user := UserModel.query.filter_by(uuid=user_uuid).first()):
             return err_resp("User not found!", 404)
-        
+
         try:
-            # TODO definir la table pour stock les token access -> modifier if en dessous
-            # TODO state parameter against csrf
             if (external := ExternalModel.query.filter_by(user_id=user.user_id).first()) is None:
                 resp = message(True, "Spotify oauth url sent")
                 resp["spotify_url"] = Spotify.oauth_url(user_uuid)
@@ -29,11 +26,11 @@ class ExternalService:
         except Exception as error:
             current_app.logger.error(error)
             return internal_err_resp()
-    
+
     @staticmethod
-    def spotify_callback(csrf,code,user_uuid):
+    def spotify_callback(csrf, code, user_uuid):
         """ Store Spotify access and refresh token """
-        if decode_token(csrf)['identity'] != user_uuid :
+        if decode_token(csrf)['identity'] != user_uuid:
             return err_resp("CSRF invalid!", 404)
         if not (user := UserModel.query.filter_by(uuid=user_uuid).first()):
             return err_resp("User not found!", 404)
@@ -42,26 +39,26 @@ class ExternalService:
             return validation_error(False, "Spotify Oauth is already done.")
         try:
             token_info = Spotify.get_tokens(code)
-            print(token_info)
+
             new_external = ExternalModel(
-                service_name= 'Spotify',
-                user_id = user.user_id,
-                access_token = token_info['access_token'],
-                refresh_token = token_info['refresh_token']
-                )
-            
+                service_name='Spotify',
+                user_id=user.user_id,
+                access_token=token_info['access_token'],
+                refresh_token=token_info['refresh_token']
+            )
+
             db.session.add(new_external)
             db.session.flush()
 
             # Commit changes to DB
             db.session.commit()
-            
+
             resp = message(True, "Spotify tokens stored")
             return resp, 201
         except Exception as error:
             current_app.logger.error(error)
             return internal_err_resp()
-    
+
     @staticmethod
     def get_spotify_data(user_uuid, app):
         """ Get spotify data :  """
@@ -69,23 +66,24 @@ class ExternalService:
             if not (user := UserModel.query.filter_by(uuid=user_uuid).first()):
                 return err_resp("User not found!", 404)
             if not (external := ExternalModel.query.filter_by(user_id=user.user_id).first()):
-                    return err_resp("External service not found!", 404)
+                return err_resp("External service not found!", 404)
             try:
-                token_info=Spotify.refresh_token(external.refresh_token)
+                token_info = Spotify.refresh_token(external.refresh_token)
                 external.access_token = token_info['access_token']
-                
+
                 db.session.add(external)
 
                 db.session.commit()
-                data=[] 
-                
+                data = []
+
                 data.append(Spotify.get_user_top_track(external.access_token))
-                data.append(Spotify.get_user_recently_played(external.access_token))
+                data.append(Spotify.get_user_recently_played(
+                    external.access_token))
                 for item in Spotify.get_user_playlist(external.access_token):
                     data.append(item['tracks'])
 
                 for d in data:
-                    for line in d :
+                    for line in d:
                         if TrackModel.query.filter_by(spotify_id=line['spotify_id']).first() is None:
                             new_track = TrackModel(
                                 artist_name=" & ".join(line['artist_name']),
@@ -98,21 +96,24 @@ class ExternalService:
                             db.session.add(new_track)
                             db.session.flush()
                 db.session.commit()
-                
+
                 for d in data:
-                    for line in d :
-                        track = TrackModel.query.filter_by(spotify_id=line['spotify_id']).first()
-                        if ((meta:=MetaUserTrackModel.query.filter_by(track_id=track.track_id).first()) is None )  : # TODO check played_at
+                    for line in d:
+                        track = TrackModel.query.filter_by(
+                            spotify_id=line['spotify_id']).first()
+                        # TODO check played_at
+                        if ((meta := MetaUserTrackModel.query.filter_by(track_id=track.track_id).first()) is None):
                             new_meta_user_track = MetaUserTrackModel(
-                                user_id = user.user_id,
-                                track_id = track.track_id,
-                                play_count = 1,
-                                last_played_date = dateutil.parser.isoparse(line['played_at'][:-1]) if "played_at" in line.keys() else None
+                                user_id=user.user_id,
+                                track_id=track.track_id,
+                                play_count=1,
+                                last_played_date=dateutil.parser.isoparse(
+                                    line['played_at'][:-1]) if "played_at" in line.keys() else None
                             )
                             db.session.add(new_meta_user_track)
                             db.session.flush()
                 db.session.commit()
-                
+
             except Exception as error:
                 current_app.logger.error(error)
                 return internal_err_resp()
