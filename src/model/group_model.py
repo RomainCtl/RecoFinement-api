a06d82e42c49d0091d08e8065c881deae7ccf021
Chantrel Romain <39402602+RomainCtl@users.noreply.github.com>
2020-11-06 15:27:34 +0100

    [bugfix] User cascade deletion (#28)

--- src/model/group_model.py
+++ src/model/group_model.py
@@ -2,9 +2,9 @@ from src import db
 
 InvitationModel = db.Table("invitations",
                            db.Column("user_id", db.Integer, db.ForeignKey(
-                               "user.user_id"), primary_key=True),
+                               "user.user_id", ondelete="CASCADE"), primary_key=True),
                            db.Column("group_id", db.Integer, db.ForeignKey(
-                               "group.group_id"), primary_key=True)
+                               "group.group_id", ondelete="CASCADE"), primary_key=True)
                            )
 
 
@@ -16,7 +16,8 @@ class GroupModel(db.Model):
 
     group_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(45), nullable=False)
-    owner_id = db.Column(db.Integer, db.ForeignKey("user.user_id"))
+    owner_id = db.Column(db.Integer, db.ForeignKey(
+        "user.user_id", ondelete="CASCADE"))
 
     invitations = db.relationship("UserModel", secondary=InvitationModel,
-                                  lazy="dynamic", backref=db.backref("invitations", lazy="dynamic"))
+                                  lazy="dynamic", backref=db.backref("invitations", lazy="dynamic", cascade="all,delete"), cascade="all,delete")
