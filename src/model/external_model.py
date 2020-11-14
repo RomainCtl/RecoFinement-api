bf6aa8ffababb8c9bc865b9a73079e01af80b7bd
RomainCtl <romain.chantrel35@gmail.com>
2020-11-14 20:51:43 +0100

    [bugfix] Deletion when we link our account to an external services

--- src/model/external_model.py
+++ src/model/external_model.py
@@ -1,17 +1,19 @@
 from src import db
 
+
 class ExternalModel(db.Model):
     """
     External service Model for storing user service related details
     """
     __tablename__ = "external_service"
     __table_args__ = (
-        db.UniqueConstraint('user_id', 'service_name', name='unique_userID_serviceName'),
+        db.UniqueConstraint('user_id', 'service_name',
+                            name='unique_userID_serviceName'),
     )
     service_id = db.Column(db.Integer, primary_key=True,
-                        autoincrement=True, index=True)
+                           autoincrement=True, index=True)
     user_id = db.Column(db.Integer, db.ForeignKey(
-        "user.user_id"), nullable=False)
+        "user.user_id", ondelete="CASCADE"), nullable=False)
     service_name = db.Column(db.String(45), nullable=False)
     access_token = db.Column(db.Text, default=None)
     refresh_token = db.Column(db.Text, default=None)
