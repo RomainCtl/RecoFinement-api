7a2be4fdd783828dc6b76ee4f9e187d013341790
unknown <y.perrot@outlook.fr>
2020-12-16 18:27:33 +0100

    Add user role to DB schema

--- src/model/user_model.py
+++ src/model/user_model.py
@@ -211,6 +211,13 @@ class RecommendedTrackModel(db.Model):
     engine_priority = db.Column(db.Integer)
 
 
+UserRoleModel = db.Table("user_role",
+                            db.Column("user_id", db.Integer, db.ForeignKey(
+                                "user.user_id", ondelete="CASCADE"), primary_key=True),
+                            db.Column("role_id", db.Integer, db.ForeignKey(
+                                "role.role_id"), primary_key=True)
+                            )
+
 class UserModel(db.Model):
     """
     User Model for storing user related details
@@ -257,6 +264,9 @@ class UserModel(db.Model):
 
     liked_genres = db.relationship(
         "GenreModel", secondary=LikedGenreModel, lazy="dynamic")
+
+    role = db.relationship(
+        "RoleModel", secondary=UserRoleModel, lazy="dynamic")
 
     @property
     def password(self):
